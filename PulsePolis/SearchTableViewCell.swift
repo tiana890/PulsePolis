416a860d8a89fba9a83f588f6545031aecd9d45e
tiana890 <minizayka@gmail.com>
2015-12-15 12:36:46 +0500

    2-d build

---
+++ PulsePolis/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  PulsePolis
+//
+//  Created by IMAC  on 14.12.15.
+//  Copyright © 2015 IMAC . All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PulsePolis/SettingsPlaceSelectedCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsPlaceSelectedCell.swift
+//  PulsePolis
+//
+//  Created by IMAC  on 12.12.15.
+//  Copyright © 2015 IMAC . All rights reserved.
+//
+
+import UIKit
+
+class SettingsPlaceSelectedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
